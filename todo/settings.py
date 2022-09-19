e31737456c25c24f9934a1982133df2382a4a805
alla <zzajka13@gmail.com>
2022-09-20 00:44:16 +0300

    create customuser

--- todo/settings.py
+++ todo/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'django_filters',
+
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "users.CustomUser"
